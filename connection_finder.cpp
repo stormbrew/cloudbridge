2d70753ab41cab3cf9ad03982ecd61069044485d
Megan Batty <graham@graham-battys-macbook.local>
2009-02-07 15:39:52 -0700

    Now does something much closer to what it's supposed to do. It matches BRIDGE requests to GET/POST/etc. on the backend. However, it does not do so based on Host: headers yet, but on the URL, which is very much incorrect.

--- connection_finder.cpp
+++ connection_finder.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+
 #include "connection_finder.hpp"
 #include "chat_handler.hpp"
 
@@ -9,38 +11,114 @@ void connection_finder::morph(buffered_connection &this_con, buffered_connection
 	this_con.set_client_handler(new_handler);
 }
 
+bool connection_finder::read_headers(buffered_connection &con)
+{
+	std::string line;
+	buffered_connection::iterator it = con.readline(con.read_begin(), line);
+	if (it == con.read_begin())
+		return false;
+		
+	std::string header_method, header_url, header_version;
+	std::string::iterator start = line.begin(), space;
+	
+	if ((space = std::find(start, line.end(), ' ')) == line.end())
+	{
+		error(con, 400, "Invalid Request", "Method line was unrecognized.");
+		return false;
+	}
+	header_method = std::string(line.begin(), space);
+	std::transform(header_method.begin(), header_method.end(), header_method.begin(), toupper);
+	start = space + 1;
+	
+	if ((space = std::find(start, line.end(), ' ')) == line.end())
+	{
+		error(con, 400, "Invalid Request", "Method line was unrecognized.");
+		return false;
+	}
+	url = std::string(start, space);
+	start = space + 1;
+	
+	header_version = std::string(start, line.end());
+	if (header_version != "HTTP/1.0" && header_version != "HTTP/1.1")
+	{
+		error(con, 400, "Invalid Request", "Unknown HTTP Version");
+		return false;
+	}
+
+	std::list<std::pair<std::string, std::string> > read_headers;
+	// read in and handle all headers
+	buffered_connection::iterator last_it = it;
+	while (true)
+	{
+		it = con.readline(it, line);
+		if (it == last_it) // no more input available, so bail. Retry again when there's more data.
+			return false;
+			
+		if (line == "") // end of headers, set our data to the class and return.
+		{
+			std::swap(method, header_method);
+			std::swap(url, header_url);
+			std::swap(headers, read_headers);
+
+			// if this is a bridge request, we want move the read position forward so when the chat_handler
+			// takes over we don't spew a bridge request at the client.
+			if (method == "BRIDGE")
+				con.set_read_begin(it);
+
+			return true;
+		}
+		
+		std::pair<std::string, std::string> header;
+		std::string::iterator delim;
+
+		start = line.begin();
+		delim = std::find(start, line.end(), ':');
+		if (delim == line.end())
+		{
+			error(con, 400, "Invalid Request", "Malformed Header.");
+			return false;
+		}
+		header.first = std::string(start, delim);
+		while (*(++delim) == ' '); // skip all spaces between : and value
+		header.second = std::string(delim, line.end());
+		read_headers.push_back(header);
+		
+		last_it = it;
+	}
+	return false;
+}
+
 void connection_finder::data_readable(buffered_connection &con)
 {
 	if (mapped)
 		return; // ignore input, let the chat handler pick it up later.
-	
-	std::string line;
-	buffered_connection::iterator it = con.readline(con.read_begin(), line);
-	if (it != con.read_begin())
+
+	if (read_headers(con))
 	{
-		// update read buffer position
-		con.set_read_begin(it);
+		bool bridge_request = method == "BRIDGE";
 		
-		printf("Incoming connection mapped to %s\n", line.c_str());
-		
-		// see if there's already a connection with this name attached
-		listen_handler::connection_map::iterator it = pool->find(line);
-		if (it != pool->end())
+		// TODO: this is wrong, it should be using Host headers.
+		connection_pool::connection other_con;
+		if (bridge_request)
+			other_con = pool->find_client(url);
+		else
+			other_con = pool->find_bridge(url);
+			
+		if (other_con)
 		{
-			buffered_connection::ptr other_con = it->second.lock();
-			if (other_con)
-			{
-				std::tr1::shared_ptr<connection_finder> other_finder = other_con->get_client_handler<connection_finder>();
-			
-				morph(con, other_con);
-				other_finder->morph(*other_con, con.shared_from_this());
-			
-				pool->erase(it);
-				return;
-			}
+			std::tr1::shared_ptr<connection_finder> other_finder = other_con->get_client_handler<connection_finder>();
+
+			morph(con, other_con);
+			other_finder->morph(*other_con, con.shared_from_this());
+			return;
 		}
-		(*pool)[line] = con.shared_from_this();
-		mapped = std::tr1::shared_ptr<std::string>(new std::string(line));
+		// only register if we get here.
+		if (bridge_request)
+			pool->register_bridge(url, con.shared_from_this());
+		else
+			pool->register_client(url, con.shared_from_this());
+			
+		mapped = true;
 	}
 }
 void connection_finder::socket_shutdown(buffered_connection &con)
@@ -52,4 +130,49 @@ void connection_finder::socket_shutdown(buffered_connection &con)
 void connection_finder::socket_close(buffered_connection &con, int err)
 {
 	printf("Socket closed, errno: 0x%x\n", err);
+}
+
+void connection_finder::error(evx::buffered_connection &con, int error_number, const std::string &name, const std::string &text)
+{
+	std::stringstream str;
+	str << "HTTP/1.1 " << error_number << " " << name << "\r\n";
+	str << "Connection: close\r\n";
+	str << "Content-Length: " << text.length() << "\r\n";
+	str << "\r\n";
+	str << text;
+	con.write(str.str());
+	con.shutdown();
+}
+
+connection_pool::connection 
+connection_pool::find_in(const std::string &host, connection_pool::connection_host_map &map)
+{
+	connection_host_map::iterator host_it = map.find(host);
+	if (host_it != map.end())
+	{
+		connection_list &list = host_it->second;
+		connection_list::iterator con_it = list.begin();
+		while (con_it != list.end())
+		{
+			connection con = con_it->lock();
+			if (!con || con->closed())
+			{
+				// remove the dead item from the list and move on.
+				con_it = list.erase(con_it);
+			} else {
+				return con;
+			}
+		}
+	}
+	return connection();
+}
+
+void
+connection_pool::register_in(const std::string &host, connection_pool::connection con, connection_pool::connection_host_map &map)
+{
+	connection_host_map::iterator host_it = map.find(host);
+	if (host_it == map.end())
+		host_it = map.insert(connection_host_map::value_type(host, connection_list())).first;
+		
+	host_it->second.push_back(con);
 }
--- listen_handler.hpp
+++ listen_handler.hpp
@@ -5,21 +5,20 @@
 
 #include "evx/buffered_connection.hpp"
 
+#include "connection_finder.hpp"
+
 class listen_handler
 {
-public:
-	typedef std::map<std::string, evx::buffered_connection::weak_ptr> connection_map;
-	
 private:
 	int listen_socket;
 	struct ev_loop *loop;
 	evx_io *watcher;
 	
-	std::tr1::shared_ptr<connection_map> waiting_connections;
+	connection_pool::ptr connections;
 	
 public:
 	listen_handler(int c_listen_socket)
-	 : listen_socket(c_listen_socket), waiting_connections(new connection_map)
+	 : listen_socket(c_listen_socket), connections(new connection_pool)
 	{}
 	
 	void operator()(struct ev_loop *loop, evx_io *watcher, int revents);
