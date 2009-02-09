bba79fbc97788c599334d985afd6b2a08d4499ed
Megan Batty <graham@graham-battys-macbook.local>
2009-02-08 20:55:49 -0700

    Made the cloudserver test a little more robust.

--- ruby/test/test_cloudserver.rb
+++ ruby/test/test_cloudserver.rb
@@ -3,7 +3,14 @@ $: << "../lib"
 require "cloudserver"
 
 server = CloudServer.new("127.0.0.1", "5432")
-client = server.accept
-while (line = client.gets)
-	puts(line)
-end
+threads = []
+5.times {|threadid|
+	threads.push Thread.new {
+		puts("Thread #{threadid} started")
+		while (client = server.accept)
+			client.write("HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: 6\r\n\r\nBlah #{threadid}")		
+			client.close
+		end
+	}
+}
+threads.each {|thread| thread.join }
