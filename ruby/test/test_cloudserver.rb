9d5d1d31a59045e79b2a1ded38d3adc1d7daab62
Megan Batty <graham@graham-battys-macbook.local>
2009-02-08 21:52:51 -0700

    Made test_cloudserver decide which server to use based on command line input, not hard coded.

--- ruby/test/test_cloudserver.rb
+++ ruby/test/test_cloudserver.rb
@@ -2,9 +2,9 @@ $: << "../lib"
 
 require "cloudserver"
 
-server = CloudServer.new("127.0.0.1", "5432")
+server = CloudServer.new(ARGV[0], "5432")
 threads = []
-5.times {|threadid|
+ARGV[1].to_i.times {|threadid|
 	threads.push Thread.new {
 		puts("Thread #{threadid} started")
 		while (client = server.accept)
