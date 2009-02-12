725010e28b1fabca7f47de13e79e395511584aa3
graham@graham-battys-macbook.local <graham@graham-battys-macbook.local>
2009-02-11 23:17:57 -0700

    Added command line arguments, including the ability to specify what host and port to listen on and what to use as the secret key (though this really needs to be in a file instead of a command line argument).

--- cloudbridge.cpp
+++ cloudbridge.cpp
@@ -1,7 +1,10 @@
 #include <sys/socket.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
+#include <iostream>
 #include <map>
 #include <string>
 #include <tr1/unordered_set>
@@ -12,32 +15,115 @@
 #include "listen_handler.hpp"
 #include "connection_finder.hpp"
 #include "chat_handler.hpp"
+#include "util.hpp"
 
 using namespace evx;
 
-int main()
+int usage(const std::string &exec_name)
 {
-	struct sockaddr_in listen_addr = {0};
-	int listen_socket = socket(PF_INET, SOCK_STREAM, 0);
+	std::cout << exec_name << " [address1]:[port1] .. [addressN]:[portN] [-h] [-s host_secret_key]" << std::endl;
+	std::cout << std::endl;
+	std::cout << "\t-h\tShow this help screen." << std::endl;
+	std::cout << "\t-s\tSet the secret key required for backends to authenticate." << std::endl;
 	
-	int on = 1;
-	setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+	return 1;
+}
+
+int main(int argc, char **argv)
+{
+	std::list<std::string> addresses;
+	std::string host_secret_key;
+	std::string exename = argv[0];
 	
-	listen_addr.sin_family = AF_INET;
-	listen_addr.sin_port = htons(8079);
-	bind(listen_socket, reinterpret_cast<struct sockaddr*>(&listen_addr), sizeof(listen_addr));
-	listen(listen_socket, 5);
+	argc--;
+	argv++;
+
+	while (argc && argv[0][0] != '-')
+	{
+		addresses.push_back(argv[0]);
+		argc--;
+		argv++;
+	}
+	
+	if (addresses.size() < 1)
+	{
+		addresses.push_back("*:8079");
+	}
+	
+	char flag;
+	while ((flag = getopt(argc - 1, argv + 1, "s:h")) != -1)
+	{
+		switch (flag)
+		{
+		case 's':
+			host_secret_key = optarg;
+			break;
+			
+		case 'h':
+		case '?':
+		default:
+			return usage(exename);
+		}
+	}
 	
 	struct ev_loop *loop = ev_default_loop(0);
-	
 	printf("Event Loop initialized: Using backend %d\n", ev_backend(loop));
+
+	std::list<evx_io> listen_watchers;
+	connection_pool::ptr connections(new connection_pool);
 	
-	std::string host_key_secret = "blahblahblorp";
-	
-	evx_io listen_watcher;
-	evx_init(&listen_watcher, listen_handler(listen_socket, host_key_secret));
-	ev_io_set(&listen_watcher, listen_socket, EV_READ);
-	ev_io_start(loop, &listen_watcher);
+	for (std::list<std::string>::iterator it = addresses.begin(); it != addresses.end(); it++)
+	{
+		std::vector<std::string> addrport = split(*it, ":", 2);
+		if (addrport.size() != 2)
+		{
+			printf("Could not parse address %s\n", it->c_str());
+			return usage(exename);
+		}
+		
+		struct addrinfo hints = {0};
+		struct addrinfo *addrinfo0, *addrinfo;
+		hints.ai_socktype = SOCK_STREAM;
+		hints.ai_flags = AI_PASSIVE;
+		
+		int err;
+		if (err = getaddrinfo(addrport[0]!="*"? addrport[0].c_str() : NULL, addrport[1].c_str(), &hints, &addrinfo0))
+		{
+			printf("Error getting address information for %s: %s\n", it->c_str(), gai_strerror(err));
+			return usage(exename);
+		}
+		
+		for (addrinfo = addrinfo0; addrinfo; addrinfo = addrinfo->ai_next)
+		{
+			int listen_socket = socket(addrinfo->ai_family, addrinfo->ai_socktype, addrinfo->ai_protocol);
+			if (-1 == listen_socket)
+			{
+				printf("Error creating socket. Errno: %d\n", errno);
+				return usage(exename);
+			}
+
+			int on = 1;
+			setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+			if (bind(listen_socket, addrinfo->ai_addr, addrinfo->ai_addrlen))
+			{
+				printf("Error binding socket. Errno: %d\n", errno);
+				return usage(exename);
+			}
+			if (listen(listen_socket, 1024))
+			{
+				printf("Error listening on socket. Errno: %d\n", errno);
+				return usage(exename);
+			}
+
+			listen_watchers.push_back(evx_io());
+			evx_io &listen_watcher = listen_watchers.back();
+			
+			evx_init(&listen_watcher, listen_handler(listen_socket, host_secret_key, connections));
+			ev_io_set(&listen_watcher, listen_socket, EV_READ);
+			ev_io_start(loop, &listen_watcher);
+		}
+	}	
 	
 	buffered_connection::register_cleanup(loop);
 	
