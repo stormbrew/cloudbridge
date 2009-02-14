62ebc919d1cb15b854e48aadbf970acac1d9eb41
graham@graham-battys-macbook.local <graham@graham-battys-macbook.local>
2009-02-13 17:49:52 -0700

    Implemented client side of dealing with limited connections.

--- rubygem/cloudbridge/lib/cloudserver.rb
+++ rubygem/cloudbridge/lib/cloudserver.rb
@@ -20,58 +20,63 @@ module CloudBridge
 		end
 	
 		def accept()
-			# Connect to the cloudbridge and let it know we're available for a connection.
-			# This is all entirely syncronous.
 			begin
-				socket = TCPSocket.new(@cloud_host, @cloud_port)
-				@sockopts.each {|opt| socket.setsockopt(*opt) }
-			rescue Errno::ECONNREFUSED
-				sleep(0.5)
-				retry
-			end
-			socket.write("BRIDGE / HTTP/1.1\r\n")
-			@listen_hosts.each {|host|
-				socket.write("Host: #{host}\r\n")
-			}
-			@listen_keys.each {|key|
-				socket.write("Host-Key: #{key}\r\n")
-			}
-			socket.write("\r\n")
-			code = nil
-			name = nil
-			headers = []
-			while (line = socket.gets())
-				line = line.strip
-				if (line == "")
-					case code.to_i
-					when 100 # 100 Continue, just a ping. Ignore.
-						code = name = nil
-						headers = []
-						next
-					when 101 # 101 Upgrade, successfuly got a connection.
-						socket.write("HTTP/1.1 100 Continue\r\n\r\n") # let the server know we're still here.
-						return socket
-					when 504 # 504 Gateway Timeout, just retry.
-						socket.close()
-						return accept()
-					else
-						raise "HTTP BRIDGE error #{code}: #{name} waiting for connection."
-					end
+				# Connect to the cloudbridge and let it know we're available for a connection.
+				# This is all entirely syncronous.
+				begin
+					socket = TCPSocket.new(@cloud_host, @cloud_port)
+					@sockopts.each {|opt| socket.setsockopt(*opt) }
+				rescue Errno::ECONNREFUSED
+					sleep(0.5)
+					retry
 				end
-			
-				if (!code && !name) # This is the initial response line
-					if (match = line.match(%r{^HTTP/1\.[01] ([0-9]{3,3}) (.*)$}))
-						code = match[1]
-						name = match[2]
-						next
-					else
-						raise "Parse error in BRIDGE request reply."
+				socket.write("BRIDGE / HTTP/1.1\r\n")
+				@listen_hosts.each {|host|
+					socket.write("Host: #{host}\r\n")
+				}
+				@listen_keys.each {|key|
+					socket.write("Host-Key: #{key}\r\n")
+				}
+				socket.write("\r\n")
+				code = nil
+				name = nil
+				headers = []
+				while (line = socket.gets())
+					line = line.strip
+					if (line == "")
+						case code.to_i
+						when 100 # 100 Continue, just a ping. Ignore.
+							code = name = nil
+							headers = []
+							next
+						when 101 # 101 Upgrade, successfuly got a connection.
+							socket.write("HTTP/1.1 100 Continue\r\n\r\n") # let the server know we're still here.
+							return socket
+						when 503, 504 # 503 Service Unavailable or 504 Gateway Timeout, just retry.
+							socket.close()
+							sleep_time = headers.find {|header| header["Retry-After"] } || 5
+							sleep(sleep_time)
+							puts("BRIDGE server timed out or is overloaded, waiting #{sleep_time}s to try again.")
+							retry
+						else
+							raise "HTTP BRIDGE error #{code}: #{name} waiting for connection."
+						end
 					end
-				else
-					if (match = line.match(%r{^(.+?):\s+(.+)$}))
-						headers.push({match[1] => match[2]})
+			
+					if (!code && !name) # This is the initial response line
+						if (match = line.match(%r{^HTTP/1\.[01] ([0-9]{3,3}) (.*)$}))
+							code = match[1]
+							name = match[2]
+							next
+						else
+							raise "Parse error in BRIDGE request reply."
+						end
 					else
-						raise "Parse error in BRIDGE request reply's headers."
+						if (match = line.match(%r{^(.+?):\s+(.+)$}))
+							headers.push({match[1] => match[2]})
+						else
+							raise "Parse error in BRIDGE request reply's headers."
+						end
 					end
 				end
 			end
